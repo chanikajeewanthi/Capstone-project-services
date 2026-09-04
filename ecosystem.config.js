e99f3514d448318444cdb3d98b3eafa5cd7d2a40
chanikajeewanthi <chanikajeewanthi16@gmail.com>
2026-09-04 13:07:18 +0530

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   apps : [
       {
+          name: "cloud-sql-auth-proxy",
+          script: "./cloud-sql-proxy ijse-cloud-storage:asia-southeast1:mysql-vm ijse-cloud-storage:asia-southeast1:postgres-vm --private-ip",
+          log_file: "./logs/cloud-sql-proxy.log",
+      },
+      {
     name   : "student-service",
     script : "java -jar student-service/target/Student-Service-1.0.0.jar",
     log_file : "./logs/student_service.log",
